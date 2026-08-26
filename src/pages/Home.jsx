a7fbd3b455ba36f3180230e0bf54c6b13123eea4
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-08-26 15:20:06 +0530

    fix: remove unused home imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import '../styles/Home.css'
-import HomeBanner from '../images/home-banner-2.png'
 import Products from '../components/Products'
 import Footer from '../components/Footer'
-import FlashSale from '../components/FlashSale'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
+const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:6001';
+
 const Home = () => {
-
   const navigate = useNavigate();
-
   const [bannerImg, setBannerImg] = useState();
 
   useEffect(()=>{
@@ -18,56 +16,45 @@ const Home = () => {
   }, [])
 
   const fetchBanner = async() =>{
-    await axios.get('http://localhost:6001/fetch-banner').then(
-      (response)=>{
-        setBannerImg(response.data);
-      }
-    )
+    try {
+      const response = await axios.get(`${API_BASE}/fetch-banner`);
+      setBannerImg(response.data);
+    } catch (error) {
+      console.error('Failed to load banner:', error);
+    }
   }
 
   return (
     <div className="HomePage">
       <div className="home-banner">
-        {bannerImg ?
-          <img src={bannerImg} alt="" />
-        :
-        ""}
+        {bannerImg ? <img src={bannerImg} alt="Store banner" /> : null}
       </div>
 
       <div className="home-categories-container">
-
         <div className="home-category-card" onClick={()=>navigate('/category/Fashion')}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZQjXpWVVQhkT_A2n03XMo2KDV4yPSLBcoNA&usqp=CAU" alt="" />
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZQjXpWVVQhkT_A2n03XMo2KDV4yPSLBcoNA&usqp=CAU" alt="Fashion" />
           <h5>Fashion</h5>
         </div>
-
         <div className="home-category-card" onClick={()=>navigate('/category/Electronics')}>
-          <img src="https://5.imimg.com/data5/ANDROID/Default/2023/1/SE/QC/NG/63182719/product-jpeg-500x500.jpg" alt="" />
+          <img src="https://5.imimg.com/data5/ANDROID/Default/2023/1/SE/QC/NG/63182719/product-jpeg-500x500.jpg" alt="Electronics" />
           <h5>Electronics</h5>
         </div>
-
         <div className="home-category-card" onClick={()=>navigate('/category/mobiles')}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3jUW7v1WFJL9Ylax9a4vazyKXwG-ktSinI4Rd7qi7MkhMr79UlIyyrNkbiK0Cz5u6WYw&usqp=CAU" alt="" />
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3jUW7v1WFJL9Ylax9a4vazyKXwG-ktSinI4Rd7qi7MkhMr79UlIyyrNkbiK0Cz5u6WYw&usqp=CAU" alt="Mobiles" />
           <h5>Mobiles</h5>
         </div>
-
         <div className="home-category-card" onClick={()=>navigate('/category/Groceries')}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXbpV_yQ_zCtZt_1kNebjvFqXvdDnLuuJPsQ&usqp=CAU" alt="" />
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXbpV_yQ_zCtZt_1kNebjvFqXvdDnLuuJPsQ&usqp=CAU" alt="Groceries" />
           <h5>Groceries</h5>
         </div>
-
         <div className="home-category-card" onClick={()=>navigate('/category/Sports-Equipment')}>
-          <img src="https://a.storyblok.com/f/112937/568x464/82f66c3a21/all_the_english-_football_terms_you_need_to_know_blog-hero-low.jpg/m/620x0/filters:quality(70)/" alt="" />
+          <img src="https://a.storyblok.com/f/112937/568x464/82f66c3a21/all_the_english-_football_terms_you_need_to_know_blog-hero-low.jpg/m/620x0/filters:quality(70)/" alt="Sports Equipment" />
           <h5>Sports Equipments</h5>
         </div>
-
       </div>
 
-
       <div id='products-body'></div>
-      <Products category = 'all'  />
-
-
+      <Products category='all' />
       <Footer />
     </div>
   )
